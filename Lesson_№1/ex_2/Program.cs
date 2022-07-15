71a34aabdac6e6bd62837cda35d663d15ce07950
Rodius <rodion.boy@mail.ru>
2022-07-15 19:43:14 +0300

    Финиш

---
+++ Lesson_№1/ex_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+if (first > second)
+{
+    Console.WriteLine("Максимальное число: " + first);
+    Console.WriteLine("Минимальное число: " + second);
+}
+if (second > first)
+{
+    Console.WriteLine("Максимальное число: " + second);
+    Console.WriteLine("Минимальное число: " + first);
+}
+if (second == first)
+{
+    Console.WriteLine("Числа равны");
+}
---
+++ Lesson_№1/ex_8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a != number)
+{
+    if (a % 2 == 0)
+    {
+        Console.Write(a + " ");
+    }
+    a++;
+}
