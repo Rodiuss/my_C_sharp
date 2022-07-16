5e588b8736a1c79e54318a09a2aa9006500b57b6
Rodius <rodion.boy@mail.ru>
2022-07-16 18:32:58 +0300

    Финиш 0.3

--- Lesson_№2/ex_15/Program.cs
+++ Lesson_№2/ex_15/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Введите день недели (от 1 до 7): ");
 int number = Convert.ToInt32(Console.ReadLine());
-if (number == 6 || number == 7) Console.WriteLine("Да");
-else if (number >= 1 && number <= 5) Console.WriteLine("Нет");
+if (number == 6 || number == 7) Console.WriteLine("Выходной");
+else if (number >= 1 && number <= 5) Console.WriteLine("Будни");
 else Console.WriteLine("Такого дня недели нет!");
