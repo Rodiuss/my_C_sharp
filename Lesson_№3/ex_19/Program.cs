293fd3b9f4c1fd04b149bf53b11f43d213a4bbf8
Rodius <rodion.boy@mail.ru>
2022-07-19 14:59:58 +0300

    Выполнено задание 19

---
+++ Lesson_№3/ex_19/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите число: ");
+string number = Console.ReadLine();
+if (number.Length > 5 || number.Length < 5) Console.WriteLine("Введите пятизначное число!");
+else if (number[0] != number[4] || number[1] != number[3]) Console.WriteLine("Это число не полиндром");
+else if (number[0] == number[4] && number[1] == number[3]) Console.WriteLine("Это чило полиндром");
