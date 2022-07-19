a7d2177e3d96737b55edebdba2616b627c56a463
Rodius <rodion.boy@mail.ru>
2022-07-19 23:41:34 +0300

    Выполнено двадцать третие задание

---
+++ Lesson_№3/ex_23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int count = Convert.ToInt32(Console.ReadLine());
+string result = String.Empty;
+
+for (int i = 1; i <= count; i++) result = result + Math.Pow(i, 2) + ", " ;
+
+Console.WriteLine("Квадраты числа " + number + ": " + result);
